1f11cd540362f14bc41bb02066efe88ce67ca8e2
2maleek <222malikibrahim@gmail.com>
2020-04-03 16:31:22 +0700

    use cors

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 const express = require('express')
-const port = process.env.PORT || 4000
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
+const cors = require('cors')
 
+const port = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded( { extended: false } ))
 app.use(cors())
-app.use(routes)
+app.use('/', routes)
 app.use(errorHandler)
 
 app.listen(port, () => {
-    console.log("Server start running on PORT " + port)
+    console.log("Server running on PORT " + port)
 })
