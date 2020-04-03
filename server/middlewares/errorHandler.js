08190f8f861a7810f19c111c3f9edec8b5cb4714
2maleek <222malikibrahim@gmail.com>
2020-04-03 16:30:41 +0700

    add errorhandler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,6 +1,11 @@
 module.exports = (err, req, res, next) => {
-    if(status.name === 'SequelizeValidationError'){
-
+    console.log(err)
+    console.log(err.status)
+    if(err.name === 'SequelizeValidationError'){
+        let errors = []
+        err.errors.forEach(element => {
+            errors.push(element.message)
+        });
     }else if(err.status === 400){
         res.status(400).json(err)
     }else if(err.status === 401){
@@ -8,8 +13,9 @@ module.exports = (err, req, res, next) => {
     }else if(err.status === 403){
         res.status(403).json(err)
     }else if(err.status === 404){
+        console.log('MAsuk 404')
         res.status(404).json(err)
     }else{
-        res.status(500).json({message: "Internal server error"})
+        res.status(500).json("Internal server error!")
     }
 }
