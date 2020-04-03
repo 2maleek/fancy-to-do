42a8d6debab3fbe3183d8ff4ff6a801b95bdb2f8
2maleek <222malikibrahim@gmail.com>
2020-04-03 16:42:49 +0700

    Wrong callbak, fix to updateTodo for update endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const Controller = require('../controllers')
 router.post('/todos', Controller.addTodo)
 router.get('/todos', Controller.getAllTodos)
 router.get('/todos/:id', Controller.getOneTodo)
-router.put('/todos/:id', Controller.getOneTodo)
+router.put('/todos/:id', Controller.updateTodo)
 router.delete('/todos/:id', Controller.deleteTodo)
 
 module.exports = router
