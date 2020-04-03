35cc72d68a67eb8bf660ac9e0fae70644d9eb502
2maleek <222malikibrahim@gmail.com>
2020-04-03 23:26:45 +0700

    add authentication & authorization, user only crud thir own todos

---
+++ server/middlewares/authorization.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const { Todo } = require('../models')
+const decode = require('../helpers/decode')
+
+module.exports = (req, res, next) => {
+  let id = Number(req.params.id)
+  let userData = decode(req.headers.access_token)
+  let UserId = userData.UserId
+
+  Todo.findOne({
+    where: { id }
+  })
+  .then(data => {
+    if(data){
+      if(data.UserId === UserId) {
+        next()
+      }else {
+        next({ status: 401, message: 'Forbidden access!'})
+      }
+    }else {
+      next({ status: 404, message: 'Todo not found! '})
+    }
+  })
+  .catch(err => {
+    next(err)
+  })
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 const Controller = require('../controllers')
+const authentication = require('../middlewares/authentication')
+const authorization  = require('../middlewares/authorization')
 
+router.post('/login', Controller.login)
+router.post('/register', Controller.register)
+
+router.use(authentication)
 router.post('/todos', Controller.addTodo)
 router.get('/todos', Controller.getAllTodos)
-router.get('/todos/:id', Controller.getOneTodo)
-router.put('/todos/:id', Controller.updateTodo)
-router.delete('/todos/:id', Controller.deleteTodo)
+
+router.get('/todos/:id', authorization, Controller.getOneTodo)
+router.put('/todos/:id', authorization, Controller.updateTodo)
+router.delete('/todos/:id', authorization, Controller.deleteTodo)
 
 module.exports = router
