0f46411bbb3dcf99e8d37f514bb555e7ef0b4b7d
2maleek <222malikibrahim@gmail.com>
2020-04-07 13:36:55 +0700

    add new endpoint: /users /projects /members

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,11 +7,17 @@ router.post('/login', Controller.login)
 router.post('/register', Controller.register)
 router.post('/googleSignIn', Controller.googleSignIn)
 router.post('/githubSignIn', Controller.githubSignIn)
+
 router.use(authentication)
+router.post('/user', Controller.getUser)
+router.get('/user', Controller.getInfoUser)
+router.post('/members', Controller.addMember)
+router.get('/members', Controller.getAllMember)
+
+router.get('/projects', Controller.getAllProjects)
+
 router.post('/todos', Controller.addTodo)
 router.get('/todos', Controller.getAllTodos)
-router.get('/users', Controller.getAllUser)
-
 router.get('/todos/:id', authorization, Controller.getOneTodo)
 router.put('/todos/:id', authorization, Controller.updateTodo)
 router.delete('/todos/:id', authorization, Controller.deleteTodo)
